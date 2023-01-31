20467fd7bc324c62ffd09cebb68c7fb624c1eeee
Ryo HAYASHI <ryo88c@gmail.com>
2023-01-31 11:36:48 +0900

    Initial implementation.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # html5-linter
+
+This is Linter for HTML5.
+
+# How to set-up
+
+```
+npm i
+```
+
+# How to use
+
+```
+node lint.js http://example.com
+```
---
+++ lint.js
@@ -0,0 +1,40 @@
+const puppeteer = require('puppeteer');
+const html5Lint = require('html5-lint');
+
+function typeOfColor(type)
+{
+    switch (type) {
+        case 'error':
+            return '\x1b[31m';
+        case 'warn':
+            return '\x1b[33m';
+    }
+    return '';
+}
+
+try {
+    if (typeof process.argv[2] !== 'string') {
+        throw new Error('The URL to evaluate must be given as an argument.');
+    }
+    new URL(process.argv[2]);
+
+    (async () => {
+        const browser = await puppeteer.launch({ headless: true });
+        const page = await browser.newPage();
+        await page.goto(process.argv[2]);
+        const html = await page.$eval('html', item => item.outerHTML);
+        await browser.close();
+
+        html5Lint(html, function (err, results) {
+            results.messages.forEach(function (msg) {
+                let type = msg.type;
+                if (typeof console[type] !== 'function') {
+                    type = 'log';
+                }
+                console[type]('%sHTML5 Lint [%s]: %s\x1b[39m', typeOfColor(type), msg.type, msg.message);
+            });
+        });
+    })();
+} catch (err) {
+    console.error(err);
+}
